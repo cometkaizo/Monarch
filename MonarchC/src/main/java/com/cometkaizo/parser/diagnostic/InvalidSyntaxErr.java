744941dd5d1e61f8c23f365f344f3877f19c0fff
cometkaizo <andy.w.andmars@gmail.com>
2025-05-13 23:58:14 -0400

    Fixed syntax error messages displaying the line after the line with the error if the error occurred on the last character of a line

--- MonarchC/src/main/java/com/cometkaizo/parser/diagnostic/InvalidSyntaxErr.java
+++ MonarchC/src/main/java/com/cometkaizo/parser/diagnostic/InvalidSyntaxErr.java
@@ -7,8 +7,16 @@ public class InvalidSyntaxErr implements Diagnostic {
     protected final String message;
     protected final int index;
 
-    public InvalidSyntaxErr(String message, CharIterator chars) {
-        int index = chars.cursor() + 1;
+    public InvalidSyntaxErr(String message, int startIndex, CharIterator chars) {
+        int index = chars.cursor();
+
+        // rewind to prev line if we started on a prev line and
+        // there is only whitespace before the cursor on this line
+        int lineIndex = chars.getLine();
+        if (lineIndex > chars.getLineAt(startIndex)) {
+            while (Character.isWhitespace(chars.get(index)) && chars.getLine() == lineIndex) index--;
+        }
+
         String prefix = "Invalid syntax ";
         String position = "(" + (chars.getLineAt(index) + 1) + ":" + (chars.getColAt(index) + 1) + ")";
         String indent = "    ";
@@ -16,7 +24,7 @@ public class InvalidSyntaxErr implements Diagnostic {
         String line = chars.getFullLineAt(index).stripTrailing();
         String trimmedLine = line.stripLeading();
         int trimAmt = line.length() - trimmedLine.length();
-        int caretPos = chars.getColAt(index) - trimAmt + indent.length();
+        int caretPos = chars.getColAt(index) + 1 - trimAmt + indent.length();
 
         this.index = index;
         this.message = message + ":\n" +
