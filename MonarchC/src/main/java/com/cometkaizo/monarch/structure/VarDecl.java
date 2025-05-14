a904cd012208ea8b7f72fe671ba43de875e96676
cometkaizo <andy.w.andmars@gmail.com>
2025-05-14 00:01:44 -0400

    Changed to use TypeGet structures to support all types of type names, including reference types

--- MonarchC/src/main/java/com/cometkaizo/monarch/structure/VarDecl.java
+++ MonarchC/src/main/java/com/cometkaizo/monarch/structure/VarDecl.java
@@ -6,6 +6,7 @@ import com.cometkaizo.bytecode.AssembleContext;
 import com.cometkaizo.monarch.structure.diagnostic.DuplicateVarErr;
 import com.cometkaizo.monarch.structure.diagnostic.NoResourcesErr;
 import com.cometkaizo.monarch.structure.diagnostic.UnknownTypeErr;
+import com.cometkaizo.monarch.structure.diagnostic.WrongTypeErr;
 import com.cometkaizo.monarch.structure.resource.Type;
 import com.cometkaizo.monarch.structure.resource.Var;
 import com.cometkaizo.monarch.structure.resource.Vars;
@@ -14,6 +15,7 @@ import com.cometkaizo.parser.Structure;
 
 public class VarDecl {
     public static class Parser extends Structure.Parser<Raw> {
+        private final Any typeParsers = new Any();
         @Override
         protected Result parseImpl(ParseContext ctx) {
             var raw = new Raw();
@@ -29,8 +31,9 @@ public class VarDecl {
             // type
             if (!ctx.literal(":")) return fail();
             ctx.whitespace();
-            raw.type = ctx.word();
-            if (raw.type == null) return fail();
+            var type = typeParsers.parse(ctx);
+            if (!type.hasValue()) return fail();
+            raw.type = type.valueNonNull();
             ctx.whitespace();
 
             if (!ctx.literal(";")) return fail();
@@ -38,9 +41,28 @@ public class VarDecl {
 
             return success(raw);
         }
+
+        protected boolean parseSettingsImpl(ParseContext ctx) {
+            if (!ctx.literal("(")) return false;
+
+            do {
+                ctx.whitespace();
+
+                var typeParserName = ctx.word();
+                if (typeParserName == null) return false;
+                typeParsers.add(typeParserName, ctx);
+
+                ctx.whitespace();
+            } while (ctx.literal(","));
+
+            if (ctx.literal(")")) return true;
+            ctx.whitespace();
+            return false;
+        }
     }
     public static class Raw extends Structure.Raw<Analysis> {
-        public String name, type;
+        public String name;
+        public Structure.Raw<?> type;
         @Override
         protected Analysis analyzeImpl(AnalysisContext ctx) {
             return new Analysis(this, ctx);
@@ -53,12 +75,13 @@ public class VarDecl {
         public Analysis(Raw raw, AnalysisContext ctx) {
             super(raw, ctx);
             this.name = raw.name;
-            var type = ctx.getType(raw.type);
-            if (type.isPresent()) this.type = type.get();
-            else {
-                ctx.report(new UnknownTypeErr(raw.type), this);
-                this.type = null;
-            }
+
+            Type type = null;
+            if (raw.type.analyze(ctx) instanceof TypeGet.Analysis typeGet) {
+                if (typeGet.type() != null) type = typeGet.type();
+                else ctx.report(new UnknownTypeErr(typeGet.name()), this);
+            } else ctx.report(new WrongTypeErr("variable type", "valid type"), this);
+            this.type = type;
 
             var m = ancestors.ofType(StackResource.Manager.class);
             if (m.isPresent()) {
--- MonarchC/src/main/java/com/cometkaizo/monarch/structure/VarParamDecl.java
+++ MonarchC/src/main/java/com/cometkaizo/monarch/structure/VarParamDecl.java
@@ -6,6 +6,7 @@ import com.cometkaizo.bytecode.AssembleContext;
 import com.cometkaizo.monarch.structure.diagnostic.DuplicateVarErr;
 import com.cometkaizo.monarch.structure.diagnostic.NoResourcesErr;
 import com.cometkaizo.monarch.structure.diagnostic.UnknownTypeErr;
+import com.cometkaizo.monarch.structure.diagnostic.WrongTypeErr;
 import com.cometkaizo.monarch.structure.resource.Type;
 import com.cometkaizo.monarch.structure.resource.Var;
 import com.cometkaizo.monarch.structure.resource.Vars;
@@ -16,6 +17,7 @@ import com.cometkaizo.util.NoPrint;
 
 public class VarParamDecl {
     public static class Parser extends Structure.Parser<Raw> {
+        private final Any typeParsers = new Any();
         @Override
         protected Result parseImpl(ParseContext ctx) {
             var raw = new Raw();
@@ -28,15 +30,35 @@ public class VarParamDecl {
             // type
             if (!ctx.literal(":")) return fail();
             ctx.whitespace();
-            raw.type = ctx.word();
-            if (raw.type == null) return fail();
+            var type = typeParsers.parse(ctx);
+            if (!type.hasValue()) return fail();
+            raw.type = type.valueNonNull();
             ctx.whitespace();
 
             return success(raw);
         }
+
+        protected boolean parseSettingsImpl(ParseContext ctx) {
+            if (!ctx.literal("(")) return false;
+
+            do {
+                ctx.whitespace();
+
+                var typeParserName = ctx.word();
+                if (typeParserName == null) return false;
+                typeParsers.add(typeParserName, ctx);
+
+                ctx.whitespace();
+            } while (ctx.literal(","));
+
+            if (ctx.literal(")")) return true;
+            ctx.whitespace();
+            return false;
+        }
     }
     public static class Raw extends Structure.Raw<Analysis> {
-        public String name, type;
+        public String name;
+        public Structure.Raw<?> type;
         @Override
         protected Analysis analyzeImpl(AnalysisContext ctx) {
             return new Analysis(this, ctx);
@@ -49,12 +71,13 @@ public class VarParamDecl {
         protected Analysis(Raw raw, AnalysisContext ctx) {
             super(raw, ctx);
             this.name = raw.name;
-            var type = ctx.getType(raw.type);
-            if (type.isPresent()) this.type = type.get();
-            else {
-                ctx.report(new UnknownTypeErr(raw.type), this);
-                this.type = null;
-            }
+
+            Type type = null;
+            if (raw.type.analyze(ctx) instanceof TypeGet.Analysis typeGet) {
+                if (typeGet.type() != null) type = typeGet.type();
+                else ctx.report(new UnknownTypeErr(typeGet.name()), this);
+            } else ctx.report(new WrongTypeErr("variable type", "valid type"), this);
+            this.type = type;
 
             var m = ancestors.ofType(StackResource.Manager.class);
             if (m.isPresent()) {
