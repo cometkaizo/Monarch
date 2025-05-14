8aab380a002169e6e0ec7b7ace06c2c826e5c201
cometkaizo <andy.w.andmars@gmail.com>
2025-05-13 23:53:10 -0400

    Added Ref Type

--- MonarchC/src/main/java/com/cometkaizo/monarch/structure/resource/Type.java
+++ MonarchC/src/main/java/com/cometkaizo/monarch/structure/resource/Type.java
@@ -6,4 +6,20 @@ public interface Type {
     String name();
     Size footprint();
     record Static(String name, Size footprint) implements Type { }
+    record Ref(boolean targetTypeKnown, Type targetType) implements Type {
+        public Ref(Type targetType) {
+            this(true, targetType);
+        }
+        public Ref() {
+            this(false, null);
+        }
+        @Override
+        public String name() {
+            return "&" + (targetType == null ? "void" : targetType.name());
+        }
+        @Override
+        public Size footprint() {
+            return Size.ONE_PTR;
+        }
+    }
 }
