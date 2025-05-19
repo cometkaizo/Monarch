73b67b6c00152c36e015c0c7514a00fe48a23e52
cometkaizo <andy.w.andmars@gmail.com>
2025-05-19 00:21:10 -0400

    Made Float64Lit accept all double values

--- MonarchC/src/main/java/com/cometkaizo/monarch/structure/Float64Lit.java
+++ MonarchC/src/main/java/com/cometkaizo/monarch/structure/Float64Lit.java
@@ -4,7 +4,6 @@ import com.cometkaizo.analysis.AnalysisContext;
 import com.cometkaizo.analysis.Expr;
 import com.cometkaizo.analysis.Size;
 import com.cometkaizo.bytecode.AssembleContext;
-import com.cometkaizo.monarch.structure.diagnostic.NumberFormatErr;
 import com.cometkaizo.monarch.structure.resource.Type;
 import com.cometkaizo.parser.ParseContext;
 import com.cometkaizo.parser.Structure;
@@ -21,18 +20,10 @@ public class Float64Lit {
             Double value = ctx.decimal();
             if (value == null) return failExpecting("floating point value");
             if (!(ctx.literal("f64") || ctx.literal("F64"))) return failExpecting("'f64' or 'F64'");
-            if (!isFloat(value)) {
-                ctx.report(new NumberFormatErr(value, "float64"));
-                return fail();
-            }
             raw.value = value;
             ctx.whitespace();
 
             return success(raw);
-        }
-
-        private boolean isFloat(Double value) {
-            return -Float.MAX_VALUE <= value && value <= Float.MAX_VALUE;
         }
     }
     public static class Raw extends Structure.Raw<Analysis> {
